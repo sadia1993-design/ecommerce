8d39841b13e38f405983ac7eee4392bc75973a21
Nishat Sanjida <sanjidan1993@gmail.com>
2020-12-22 12:02:30 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+
+      // banner slider
+     $('.slider-area').slick({
+        autoplay: true,
+        speed: 800,
+        arrows:false,
+        dots: true,
+        fade:true
+     });
+
+    });
+
+    
+}(jQuery));
